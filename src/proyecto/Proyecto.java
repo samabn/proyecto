7653cc67e29a20c5a38b031c7eade5d817f58ddd
sam <sam.abundis82@gmail.com>
2019-05-02 12:02:20 -0500

    Comienzo

---
+++ src/proyecto/Proyecto.java
@@ -0,0 +1,16 @@
+package proyecto;
+
+import graficos.Login;
+import javax.swing.JFrame;
+
+/** @author sam **/
+
+public class Proyecto {
+
+    public static void main(String[] args) {
+        //JFrame jf = new JFrame();
+        Login l = new Login();
+        l.setVisible(true);
+    }
+    
+}
